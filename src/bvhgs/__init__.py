6aa3d2a2e60d52a67866f3af38cd3c9486e5de45
Fangjun Zhou <fangjun.zhou02@gmail.com>
2025-05-09 02:51:18 -0500

    Added slangpy math module and numpy-quaternion dependency

--- src/bvhgs/__init__.py
+++ src/bvhgs/__init__.py
@@ -8,4 +8,4 @@ SHADER_PATH = pathlib.Path(__file__).parent / "slang"
 device = spy.create_device(include_paths=[SHADER_PATH.absolute()])
 logger.info(f"Slang device created: {device}")
 
-gaussian_module = spy.Module.load_from_file(device, "gaussian.slang")
+math_module = spy.Module.load_from_file(device, "math.slang")
