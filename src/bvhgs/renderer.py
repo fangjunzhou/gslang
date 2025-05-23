335f4bf865ed2ed6004aa8bfeec40d2ba394aabe
Fangjun Zhou <fangjun.zhou02@gmail.com>
2025-05-23 04:00:08 -0700

    Added renderer implementation starter code

--- src/bvhgs/renderer.py
+++ src/bvhgs/renderer.py
@@ -3,6 +3,7 @@ import slangpy as spy
 from bvhgs import device
 from bvhgs.camera import Camera
 from bvhgs.gaussian import GaussianCloud
+from bvhgs.prefix_sum import prefix_sum
 
 
 class Renderer:
@@ -13,6 +14,15 @@ class Renderer:
     render_target: spy.Texture
 
     program: spy.ShaderProgram
+    # Projection kernels.
+    ker_proj: spy.ComputeKernel
+    ker_cull: spy.ComputeKernel
+    # Tile kernels.
+    ker_tile: spy.ComputeKernel
+    ker_gs_table: spy.ComputeKernel
+    # Rasterization kernels.
+    ker_duplicate_gs: spy.ComputeKernel
+    ker_rasterize: spy.ComputeKernel
 
     def __init__(self, gaussians: GaussianCloud, camera: Camera) -> None:
         """Constructor for the Rasterizer class.
@@ -23,9 +33,50 @@ class Renderer:
         self.gaussians = gaussians
         self.camera = camera
 
-        self.program = device.load_program(
-            "renderer.slang",
-            entry_point_names=["projection", "cull", "rasterize"],
+        # Load the module.
+        renderer_module = device.load_module("renderer.slang")
+        self.program = device.link_program(
+            modules=[renderer_module],
+            entry_points=[],
+        )
+        # Load kernels.
+        self.ker_proj = device.create_compute_kernel(
+            device.link_program(
+                modules=[renderer_module],
+                entry_points=[renderer_module.entry_point("project")],
+            )
+        )
+        self.ker_cull = device.create_compute_kernel(
+            device.link_program(
+                modules=[renderer_module],
+                entry_points=[renderer_module.entry_point("cull")],
+            )
+        )
+        self.ker_tile = device.create_compute_kernel(
+            device.link_program(
+                modules=[renderer_module],
+                entry_points=[renderer_module.entry_point("computeTile")],
+            )
+        )
+        self.ker_gs_table = device.create_compute_kernel(
+            device.link_program(
+                modules=[renderer_module],
+                entry_points=[
+                    renderer_module.entry_point("buildGaussianTable")
+                ],
+            )
+        )
+        self.ker_duplicate_gs = device.create_compute_kernel(
+            device.link_program(
+                modules=[renderer_module],
+                entry_points=[renderer_module.entry_point("duplicateGaussian")],
+            )
+        )
+        self.ker_rasterize = device.create_compute_kernel(
+            device.link_program(
+                modules=[renderer_module],
+                entry_points=[renderer_module.entry_point("rasterize")],
+            )
         )
 
         # Create a render texture for rendering.
@@ -54,5 +105,53 @@ class Renderer:
 
     def render(self) -> None:
         """Render the Gaussian points to the render target."""
-        # TODO: Implement the rendering logic.
-        pass
+        # Get the camera parameters.
+        camera_params = self.camera.to_slang()
+        # Project the Gaussian points to screen space.
+        gaussian_2d_buf = device.create_buffer(
+            element_count=len(self.gaussians),
+            struct_type=self.program.reflection.g_gaussian_2d,
+            usage=spy.BufferUsage.shader_resource
+            | spy.BufferUsage.unordered_access,
+        )
+        inside_flag_buf = device.create_buffer(
+            element_count=len(self.gaussians),
+            struct_type=self.program.reflection.g_inside_flag,
+            usage=spy.BufferUsage.shader_resource
+            | spy.BufferUsage.unordered_access,
+        )
+
+        self.ker_proj.dispatch(
+            thread_count=[len(self.gaussians), 1, 1],
+            vars={
+                "g_camera": camera_params,
+                "g_gaussian_3d": self.gaussian_3d,
+                "g_gaussian_2d": gaussian_2d_buf,
+                "g_inside_flag": inside_flag_buf,
+            },
+        )
+        # Cull the Gaussian points.
+        inside_offset_buf = prefix_sum(inside_flag_buf)
+        inside_offset_cursor = spy.BufferCursor(
+            self.program.reflection.g_inside_offset.type_layout.element_type_layout,
+            inside_offset_buf,
+        )
+        # Read the last element of the cull prefix buffer to get the number of culled points.
+        num_viewing = int(inside_offset_cursor[len(inside_offset_cursor) - 1].read())  # type: ignore
+        # Create a buffer for the culled Gaussian points.
+        culled_gaussian_2d_buf = device.create_buffer(
+            element_count=num_viewing,
+            struct_type=self.program.reflection.g_gaussian_2d,
+            usage=spy.BufferUsage.shader_resource
+            | spy.BufferUsage.unordered_access,
+        )
+        self.ker_cull.dispatch(
+            thread_count=[num_viewing, 1, 1],
+            vars={
+                "g_gaussian_2d": gaussian_2d_buf,
+                "g_inside_flag": inside_flag_buf,
+                "g_inside_offset": inside_offset_buf,
+                "g_culled_gaussian_2d": culled_gaussian_2d_buf,
+            },
+        )
+        # TODO: Implement the tile and rasterization kernels.
