7438e369c63291161e722bdc61523cc94909f2ef
Fangjun Zhou <fangjun.zhou02@gmail.com>
2025-05-23 12:47:39 -0700

    Rename radix sort

--- tests/test_radix_sort.py
+++ tests/test_radix_sort.py
@@ -6,26 +6,27 @@ import pytest
 import slangpy as spy
 from bvhgs import device
 from bvhgs.radix_sort import radix_sort
-from bvhgs import radix_sort_module
+
 
 @pytest.fixture(params=[1, 16, 64, 255, 256, 257])
 def n(request):
     return request.param
 
+
 def test_radix_sort(n):
-    keys   = np.random.randint(0, 256, size=n, dtype=np.uint64)
+    keys = np.random.randint(0, 256, size=n, dtype=np.uint64)
     values = np.arange(n, dtype=np.uint32)
 
-    mod      = device.load_module("radix-sort.slang")
+    mod = device.load_module("radix-sort.slang")
     prog_bld = device.link_program([mod], [mod.entry_point("buildHist")])
-    tuple_type  = prog_bld.reflection.buildHist.state.src
+    tuple_type = prog_bld.reflection.buildHist.state.src
     elem_layout = tuple_type.type_layout.element_type_layout
 
     src_buf = device.create_buffer(
         element_count=n,
         struct_type=tuple_type,
         usage=spy.BufferUsage.shader_resource
-              | spy.BufferUsage.unordered_access,
+        | spy.BufferUsage.unordered_access,
     )
 
     src_cur = spy.BufferCursor(elem_layout, src_buf)
@@ -35,10 +36,10 @@ def test_radix_sort(n):
 
     sorted_buf, hist_buf = radix_sort(src_buf)
 
-    dst_cur  = spy.BufferCursor(elem_layout, sorted_buf)
+    dst_cur = spy.BufferCursor(elem_layout, sorted_buf)
     out_keys, out_vals = [], []
     for i in range(n):
-        kv = cast(Dict[str,int], dst_cur[i].read())
+        kv = cast(Dict[str, int], dst_cur[i].read())
         out_keys.append(kv["_0"])
         out_vals.append(kv["_1"])
 
