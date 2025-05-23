8797a0bdf6351b042ff2a28b8b3058f7b3064708
Oliviaaaaa-Feng <olivia.feng19@gmail.com>
2025-05-22 23:28:39 -0700

    radix sort

--- src/bvhgs/radix_sort.py
+++ src/bvhgs/radix_sort.py
@@ -2,7 +2,7 @@ import slangpy as spy
 from bvhgs import device
 from bvhgs.prefix_sum import prefix_sum
 
-def radix_sort(src_buf, mask: int = 0xFF, max_bits: int = 64):
+def radix_sort(src_buf, mask: int = 0x3F):
     mod      = device.load_module("radix-sort.slang")
     prog_clr = device.link_program([mod], [mod.entry_point("clearHist")])
     prog_bld = device.link_program([mod], [mod.entry_point("buildHist")])
@@ -13,6 +13,7 @@ def radix_sort(src_buf, mask: int = 0xFF, max_bits: int = 64):
     k_scatter = device.create_compute_kernel(prog_sct)
 
     n = src_buf.size // src_buf.struct_size
+    padded_n  = ((n + 63) // 64) * 64
     dst_buf = device.create_buffer(
         element_count=n,
         struct_type=prog_clr.reflection.clearHist.dst,
@@ -22,18 +23,20 @@ def radix_sort(src_buf, mask: int = 0xFF, max_bits: int = 64):
     bit_width = mask.bit_length()
     buckets   = mask + 1
 
-    for shift in range(0, max_bits, bit_width):
-        hist_buf = device.create_buffer(
-            element_count=buckets,
-            struct_type=prog_bld.reflection.buildHist.hist,
-            usage=spy.BufferUsage.shader_resource | spy.BufferUsage.unordered_access,
-        )
-        offs_buf = device.create_buffer(
-            element_count=buckets,
-            struct_type=prog_clr.reflection.clearHist.offs,
-            usage=spy.BufferUsage.shader_resource | spy.BufferUsage.unordered_access,
-        )
+    hist_buf = device.create_buffer(
+        element_count=buckets,
+        struct_type=prog_bld.reflection.buildHist.hist,
+        usage=spy.BufferUsage.shader_resource
+              | spy.BufferUsage.unordered_access,
+    )
+    offs_buf = device.create_buffer(
+        element_count=buckets,
+        struct_type=prog_clr.reflection.clearHist.offs,
+        usage=spy.BufferUsage.shader_resource
+              | spy.BufferUsage.unordered_access,
+    )
 
+    for shift in range(0, 64, bit_width):
         state = {
             "shift":       shift,
             "mask":        mask,
@@ -49,20 +52,21 @@ def radix_sort(src_buf, mask: int = 0xFF, max_bits: int = 64):
         )
 
         k_build.dispatch(
-            thread_count=[64, 1, 1],
+            thread_count=[padded_n, 1, 1],
             state=state,
             numElements=n,
         )
 
         offs_new = prefix_sum(hist_buf)
-
         state["offs"] = offs_new
+
         k_scatter.dispatch(
-            thread_count=[64, 1, 1],
+            thread_count=[padded_n, 1, 1],
             state=state,
             numElements=n,
         )
 
         src_buf, dst_buf = dst_buf, src_buf 
+        offs_buf = offs_new
 
     return src_buf
