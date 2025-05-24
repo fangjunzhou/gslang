40b1545d8e8de56814014a0908d1325148a52a72
cty288 <chentim.sh@gmail.com>
2025-05-24 04:44:53 -0700

    prefix sum benchmark and cull benchmark

--- tests/renderer/test_projection.py
+++ tests/renderer/test_projection.py
@@ -377,3 +377,93 @@ def test_projection_benchmark(benchmark, benchmark_buffer_size: int):
         setup=projection_benchmark_setup,
         rounds=10,
     )
+
+
+def test_cull_benchmark(benchmark, benchmark_buffer_size: int):
+    """Benchmark the culling of Gaussian3D based on camera parameters.
+
+    :param benchmark: The benchmark fixture.
+    :param benchmark_buffer_size: Size of the buffer for the test.
+    """
+    module = device.load_module("renderer.slang")
+    program_cull = device.link_program([module], [module.entry_point("cull")])
+    ker_cull = device.create_compute_kernel(program_cull)
+    
+    program_proj = device.link_program([module], [module.entry_point("project")])
+    ker_proj = device.create_compute_kernel(program_proj)
+
+    def cull_benchmark_setup():
+        """Setup for the culling benchmark."""
+        gaussians = GaussianCloud()
+        gaussians.randomize(benchmark_buffer_size)
+
+        # Create a camera instance.
+        cam = Camera(
+            rotation=glm.quat(1, 0, 0, 0),
+            translation=glm.vec3(0, 0, 1),
+            sensor_size=glm.uvec2(512, 512),
+            focal_length=64.0,
+            near_plane=0.5,
+            far_plane=1000.0,
+        )
+
+        gaussian_3d_buf = device.create_buffer(
+            element_count=benchmark_buffer_size,
+            struct_type=program_proj.reflection.g_gaussian_3d,
+            usage=spy.BufferUsage.shader_resource,
+        )
+        gaussian_cursor = spy.BufferCursor(
+            program_proj.reflection.g_gaussian_3d.type_layout.element_type_layout,
+            gaussian_3d_buf,
+        )
+
+        for i in range(len(gaussians)):
+            gaussian_cursor[i].write(gaussians[i])
+        gaussian_cursor.apply()
+
+        gaussian_2d_buf = device.create_buffer(
+            element_count=benchmark_buffer_size,
+            struct_type=program_proj.reflection.g_gaussian_2d,
+            usage=spy.BufferUsage.shader_resource
+            | spy.BufferUsage.unordered_access,
+        )
+
+        inside_flag_buf = device.create_buffer(
+            element_count=benchmark_buffer_size,
+            struct_type=program_proj.reflection.g_inside_flag,
+            usage=spy.BufferUsage.shader_resource
+            | spy.BufferUsage.unordered_access,
+        )
+
+        ker_proj.dispatch(
+            thread_count=[benchmark_buffer_size, 1, 1],
+            vars={
+                "g_camera": cam.to_slang(),
+                "g_gaussian_3d": gaussian_3d_buf,
+                "g_gaussian_2d": gaussian_2d_buf,
+                "g_inside_flag": inside_flag_buf,
+            },
+        )
+        args = ()
+        g_gaussian_2d_culled_buf = device.create_buffer(
+            element_count=benchmark_buffer_size,
+            struct_type=program_cull.reflection.g_gaussian_2d_culled,
+            usage=spy.BufferUsage.shader_resource
+            | spy.BufferUsage.unordered_access,
+        )
+        kwargs = {
+            "thread_count": [benchmark_buffer_size, 1, 1],
+            "vars": {
+                "g_gaussian_2d_culled": g_gaussian_2d_culled_buf,
+                "g_gaussian_2d": gaussian_2d_buf,
+                "g_inside_flag": inside_flag_buf,
+            },
+        }
+
+        return (args, kwargs)
+    
+    benchmark.pedantic(
+        ker_cull.dispatch,
+        setup=cull_benchmark_setup,
+        rounds=10,
+    )
--- tests/test_prefix_sum.py
+++ tests/test_prefix_sum.py
@@ -4,7 +4,7 @@ import pytest
 from bvhgs import device
 from bvhgs.prefix_sum import prefix_sum
 from typing import cast
-
+import pytest_benchmark
 
 @pytest.fixture(
     params=[
@@ -55,3 +55,34 @@ def test_prefix_sum(input_range):
     assert (
         actual == expected
     ), f"prefix_sum failed: got {actual}, expected {expected}"
+
+
+@pytest.fixture(params=[32, 64, 128, 1024, 2048, 4096, 8192])
+def benchmark_buffer_size(request: pytest.FixtureRequest) -> int:
+    """Fixture to provide a buffer size for benchmarking.
+
+    :param request: The pytest request object.
+    :return: The buffer size.
+    """
+    return request.param
+
+
+def test_prefix_sum_benchmark(benchmark, benchmark_buffer_size: int):
+    """Benchmark the prefix_sum function with a given buffer size."""
+    input_data = np.random.randint(0, 2, size=benchmark_buffer_size).tolist()
+    mod = device.load_module("prefix-sum.slang")
+    prog_scan = device.link_program([mod], [mod.entry_point("wave_scan")])
+    src_buf = device.create_buffer(
+        element_count=benchmark_buffer_size,
+        struct_type=prog_scan.reflection.wave_scan.src,
+        usage=spy.BufferUsage.shader_resource,
+    )
+    src_cur = spy.BufferCursor(
+        prog_scan.reflection.wave_scan.src.type_layout.element_type_layout,
+        src_buf,
+    )
+    for i, v in enumerate(input_data):
+        src_cur[i].write(int(v))
+    src_cur.apply()
+
+    benchmark(lambda: prefix_sum(src_buf))
